8e91a4c459628cb52a0ac51ae855c63047dab2f0
Victor Nicolet <victorn@cs.toronto.edu>
2021-04-22 14:02:53 -0400

    Adding benchmarks.

--- benchmarks/unimodal_lists/unimodality.ml
+++ benchmarks/unimodal_lists/unimodality.ml
@@ -18,8 +18,12 @@ and aux l = function
   | UElt a -> Cons (a, l)
   | USplit (x, a, b, y) -> aux (Cons (a, Cons (b, aux l y))) x
 
-(* The sum function on lists. *)
-let rec sum = function Nil -> 0 | Cons (hd, tl) -> hd + sum tl
+(* Invariant: unimodal list *)
+let rec is_unimodal_list = function Nil -> true | Cons (x, l) -> aux_up x l
+
+and aux_up pr = function Nil -> true | Cons (x, l) -> if pr <= x then aux_up x l else aux_down x l
+
+and aux_down pr = function Nil -> true | Cons (x, l) -> pr >= x && aux_down x l
 
 (*
   Testing a not-so-natural specification.
@@ -30,7 +34,8 @@ let rec sum = function Nil -> 0 | Cons (hd, tl) -> hd + sum tl
 let rec g = function
   | UNil -> [%synt s0]
   | UElt a -> [%synt f0] a
-  | USplit (x, a, b, y) -> if a + b = 0 then [%synt f1] (g x) (g y) else [%synt f2] a b (g x) (g y)
+  | USplit (x, a, b, y) -> [%synt f1] a b (g x) (g y)
 
 ;;
-assert (g = r @@ sum)
+assert (g = r @@ is_unimodal_list)
+(* No solution: there is a clear hint that we need a lifting !! *)
