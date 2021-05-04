7fdd5fa1f0cd334471fd069a3670b7a37f448986
Victor Nicolet <victorn@cs.toronto.edu>
2021-05-04 09:57:30 -0400

    New benchmarks, renaming zippers to tree paths.

---
+++ benchmarks/unrealizable/mits_nosum.ml
@@ -0,0 +1,20 @@
+type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
+
+let rec spec t = f 0 t
+
+and f s = function
+  | Nil -> s
+  | Node (a, l, r) ->
+      let _mts = f s l in
+      let _mts' = max (_mts + a) 0 in
+      f _mts' r
+  [@@ensures fun x -> x >= 0]
+
+let rec mits = function Nil -> [%synt s0] | Node (a, l, r) -> [%synt join1] a (mits l) (mits r)
+
+let repr x = x
+
+(* This should fail in no time. Solver returned unsat in less than a second. *)
+
+;;
+assert (mits = repr @@ spec)
--- bin/Atropos.ml
+++ bin/Atropos.ml
@@ -12,7 +12,7 @@ let print_usage () =
   pf stdout
     "Options:\n\
     \    -h --help                      Print this message.\n\
-    \    -v --verbose                   Print verbose.\n\
+    \    -d --debug                     Print debugging info.\n\
     \    -i --info-off                  Print timing information only.\n\
     \  Otimizations off/on:\n\
     \    -s --no-splitting              Do not split systems into subsystems.\n\
@@ -39,7 +39,8 @@ let print_usage () =
 let options =
   [
     ('h', "help", Some print_usage, None);
-    ('v', "verbose", set Config.debug true, None);
+    ('d', "debug", set Config.debug true, None);
+    ('v', "verbose", set Config.verbose true, None);
     ('i', "info-off", set Config.info false, None);
     ('\000', "show-vars", set Config.show_vars true, None);
     ('\000', "acegis", set Config.use_acegis true, None);
