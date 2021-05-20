2071e337bb4a5d3504684602f6ff88cead79c5e7
Victor Nicolet <victorn@cs.toronto.edu>
2021-05-20 09:59:38 -0400

    Adding examples.

---
+++ benchmarks/unrealizable/memo_max_tree.ml
@@ -0,0 +1,20 @@
+type 'a tree = Leaf of 'a | Node of 'a * 'a tree * 'a tree
+
+type 'a tree_m = Leaf' of 'a | Node' of 'a * int * 'a tree_m * 'a tree_m
+
+let rec repr = function Leaf' a -> Leaf a | Node' (a, _i, l, r) -> Node (a, repr l, repr r)
+
+let rec pred = function
+  | Leaf' _a -> true
+  | Node' (a, n, l, r) -> n = max a (max (aux l) (aux r)) && pred l && pred r
+
+and aux = function Leaf' a -> a | Node' (_a, n, _l, _r) -> n
+
+(** Reference function : contains.
+  ⚠️  This syntax is not yet supported (PMRS with a parameter).
+  *)
+let spec x t =
+  let rec aux = function Leaf a -> a == x | Node (a, l, r) -> a == x || aux l || aux r in
+  aux t
+
+(* To be continued... *)
