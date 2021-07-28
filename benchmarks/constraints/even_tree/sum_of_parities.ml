caadc9a23f1c72719325145a56ec51b6a9ceb22a
danyalette <danyalette@gmail.com>
2021-07-28 15:58:40 -0400

    Add even_tree benchmarks

---
+++ benchmarks/constraints/even_tree/sum_of_parities.ml
@@ -0,0 +1,12 @@
+type 'a tree = Elt of 'a | Node of 'a * 'a tree * 'a tree
+
+let rec is_even = function
+  | Elt a -> a mod 2 = 0
+  | Node (a, l, r) -> a mod 2 = 0 && is_even l && is_even r
+
+let rec spec = function
+  | Elt a -> a mod 2
+  | Node (a, l, r) -> (a mod 2) + spec l + spec r
+  [@@ensures fun x -> x >= 0]
+
+let rec target = function Elt a -> [%synt c0] | Node (a, l, r) -> [%synt c1] [@@requires is_even]
