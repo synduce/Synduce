30479a05c4e41856c28e0fa7348b4a86cc46d745
Victor Nicolet <victorn@cs.toronto.edu>
2021-09-09 12:48:42 -0400

    Docs update.

--- src/lang/PMRS.ml
+++ src/lang/PMRS.ml
@@ -18,9 +18,11 @@ type top_function = variable * variable list * term
 type t = {
   pvar : Variable.t;
       (**
-    The main function symbol.
+    The main function symbol, storing general information about the PMRS.
     The specification associated to the PMRS may be found using:
     [get_spec pvar]
+    The type of the PMRS (the main function of the group of mutually recursive
+    functions) is stored as the type of pvar.
   *)
   pinput_typ : RType.t list;
       (**
@@ -494,3 +496,6 @@ let depends (p : t) : t list =
   List.dedup_and_sort
     ~compare:(fun p1 p2 -> String.compare p1.pvar.vname p2.pvar.vname)
     (List.concat_map ~f (Map.data p.prules))
+
+(** Returns the output type of a PMRS. *)
+let output_type (p : t) : RType.t = snd (RType.fun_typ_unpack (Variable.vtype_or_new p.pvar))
