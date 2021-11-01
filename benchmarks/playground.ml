2f0b87c74e48a7eff0ca8b8647c3f4ff37a7122c
Victor Nicolet <victorn@cs.toronto.edu>
2021-11-01 13:44:56 -0400

    Update dependencies.

--- benchmarks/playground.ml
+++ benchmarks/playground.ml
@@ -1,44 +1,30 @@
 (** @synduce --no-lifting *)
-type clist = CNil | Single of int | Concat of int * clist * int * int * clist
+type 'a clist = CTwo of 'a * 'a | Concat of 'a clist * 'a clist
 
-type list = Nil | Cons of int * list
+type 'a list = Two of 'a * 'a | Cons of 'a * 'a list
 
-let rec spec = function
-  | Nil -> (0, 0)
-  | Cons (hd, tl) ->
-      let mps, mss = spec tl in
-      (max (mps + hd) 0, max mss (max (mps + hd) 0))
-  [@@ensures fun (mps, mss) -> mps >= 0 && mss >= 0 && mss >= mps]
+let rec repr l = c l
 
-(** Memoize sum *)
-let rec memo = 
-  function
-  | CNil -> true
-  | Single x -> true
-  | Concat(sx, x, my, sy, y) -> sx = sum x && sy = sum y && memo x && memo y && my = mps y
-and sum = 
-function 
-| CNil -> 0 
-| Single x -> x 
-| Concat (sx, x, my, sy, y) -> sum x + sum y
-and mps = function
-  | CNil -> 0 
-  | Single x -> x
-  | Concat(sx,x,my,sy,y) -> max (mps x) (sx + (mps y))
-
-let rec target t = h t [@@requires memo]
-
-and h = function
-  | CNil -> [%synt s0]
-  | Single a -> [%synt f0] a
-  | Concat (sy, y, my, sz, z) -> [%synt odot] sy sz my (h y) (h z)
-
-let rec repr t = c t
-
-and c = function CNil -> Nil | Single a -> Cons (a, Nil) | Concat (sx, x, my, sy, y) -> dec y x
+and c = function CTwo (a, b) -> Two (a, b) | Concat (x, y) -> dec y x
 
 and dec l = function
-  | CNil -> repr l
-  | Single a -> Cons (a, repr l)
-  | Concat (sx, x, my, sy, y) -> dec (Concat (sx, y, my, sy, l)) x
+  | CTwo (a, b) -> Cons (a, Cons (b, c l))
+  | Concat (x, y) -> dec (Concat (y, l)) x
+
+let rec spec l = f l
+
+and f = function
+  | Two (a, b) -> (min a b, max a b)
+  | Cons (hd, tl) -> (fun (amin, sec_min) -> (min hd amin, min sec_min (max hd amin))) (f tl)
+  [@@ensures fun (x, y) -> x <= y]
+
+let rec is_sorted l = (fun (a,b) -> b) (sorted (repr l))
+and sorted = 
+  function 
+  | Two (x,y) -> x, x > y
+  | Cons (a, l) -> let hd, sorted = sorted l in a, a > hd && sorted
+
+let rec target = function CTwo (a, b) -> [%synt f0] a b | Concat (x, y) -> [%synt odot] (target y)  [@@requires is_sorted]
+
+
 
