62d42af28eff3067f036d9949bb284e4827a0cc7
victorn <victor.nicolet@pm.me>
2022-07-06 19:40:14 -0400

    All benchmarks.

--- benchmarks/configurations_set_search.py
+++ benchmarks/configurations_set_search.py
@@ -1,134 +1,135 @@
 # REQ; share ref function invariant
 incomplete_benchmarks_set0 = [
     # BALANCED TREE
-    # ["incomplete/balanced/height.ml", "", True, 1],  # OK +
-    # ["incomplete/balanced/height_v2.ml", "", True, 1],  # OK +
-    #  ["incomplete/balanced/node_count.ml", "", True, 1],  # OK +
+    ["incomplete/balanced/height.ml", "", True, 1],  # OK +
+    ["incomplete/balanced/height_v2.ml", "", True, 1],  # OK +
+    ["incomplete/balanced/node_count.ml", "", True, 1],  # OK +
 
     # BST
-    # ["incomplete/bst/count_lt_partial_2.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/contains.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/contains_v2.ml", "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/contains_v3.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/contains_bool_2.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/contains_no_ensures.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/count_between.ml", "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/from_list_contains.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/from_list_max.ml", "", True, 1],  # OK  +
-    # ["incomplete/bst/most_frequent_v1.ml", "", True, 1],  # OK +
-    # ["incomplete/bst/minmax_separate.ml", "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/minmax_sim.ml", "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/check_mem_included.ml", "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/check_no_subtree_sum_gt2.ml",
-    #  "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/check_included_comb.ml",
-    #  "", True, 1],  # "Too many open files"
-    # ["incomplete/bst/sum_gt_by_key.ml", "", True, 1],  # OK
-    # ["incomplete/bst/sum_between.ml", "", True, 1],  # "Too many open files"
+    ["incomplete/bst/count_lt_partial_2.ml", "", True, 1],  # OK +
+    ["incomplete/bst/contains.ml", "", True, 1],  # OK +
+    ["incomplete/bst/contains_v2.ml", "", True, 1],  # "Too many open files"
+    ["incomplete/bst/contains_v3.ml", "", True, 1],  # OK +
+    ["incomplete/bst/contains_bool_2.ml", "", True, 1],  # OK +
+    ["incomplete/bst/contains_no_ensures.ml", "", True, 1],  # OK +
+    ["incomplete/bst/count_between.ml", "", True, 1],  # "Too many open files"
+    ["incomplete/bst/from_list_contains.ml", "", True, 1],  # OK +
+    ["incomplete/bst/from_list_max.ml", "", True, 1],  # OK  +
+    ["incomplete/bst/most_frequent_v1.ml", "", True, 1],  # OK +
+    ["incomplete/bst/minmax_separate.ml", "", True, 1],  # "Too many open files"
+    ["incomplete/bst/minmax_sim.ml", "", True, 1],  # "Too many open files"
+    ["incomplete/bst/check_mem_included.ml",
+     "", True, 1],  # "Too many open files"
+    ["incomplete/bst/check_no_subtree_sum_gt2.ml",
+     "", True, 1],  # "Too many open files"
+    ["incomplete/bst/check_included_comb.ml",
+     "", True, 1],  # "Too many open files"
+    ["incomplete/bst/sum_gt_by_key.ml", "", True, 1],  # OK
+    ["incomplete/bst/sum_between.ml", "", True, 1],  # "Too many open files"
 
     # COMBINE TRAVERSALS
     # First config in top-down creates too much recursion
-    #["incomplete/combine/mts.ml", "", True, 1],
-    # ["incomplete/combine/mts_and_mps.ml", "", True, 1],  # OK +
-    # ["incomplete/combine/mss_with_sum.ml", "", True, 1],  # OK +
+    ["incomplete/combine/mts.ml", "", True, 1],
+    ["incomplete/combine/mts_and_mps.ml", "", True, 1],  # OK +
+    ["incomplete/combine/mss_with_sum.ml", "", True, 1],  # OK +
 
     # COMPRESSED LIST
-    # ["incomplete/compressed_list/sum.ml", "", True, 1],  # OK +
+    ["incomplete/compressed_list/sum.ml", "", True, 1],  # OK +
 
     # CONSTANT LIST
-    # ["incomplete/constantlist/contains.ml", "", True, 1],  # OK +
-    # ["incomplete/constantlist/index_of.ml", "", True, 1],  # OK +
+    ["incomplete/constantlist/contains.ml", "", True, 1],  # OK +
+    ["incomplete/constantlist/index_of.ml", "", True, 1],  # OK +
 
     # EMPTY RIGHT SUBTREE
-    # ["incomplete/empty_right/contains.ml", "", True, 1],   # OK +
-    # ["incomplete/empty_right/sum.ml", "", True, 1],        # OK +
+    ["incomplete/empty_right/contains.ml", "", True, 1],   # OK +
+    ["incomplete/empty_right/sum.ml", "", True, 1],        # OK +
 
     # EVEN LISTS
-    # ["incomplete/evenlist/first_odd.ml", "", True, 1],  # OK +
-    # ["incomplete/evenlist/parity_of_first.ml", "", True, 1],  # OK - 4 failed
-    # ["incomplete/evenlist/parity_of_last.ml", "", True, 1],  # OK +
-    # ["incomplete/evenlist/parity_of_sum.ml", "", True, 1],  # OK - 6 failed
+    ["incomplete/evenlist/first_odd.ml", "", True, 1],  # OK +
+    ["incomplete/evenlist/parity_of_first.ml", "", True, 1],  # OK - 4 failed
+    ["incomplete/evenlist/parity_of_last.ml", "", True, 1],  # OK +
+    ["incomplete/evenlist/parity_of_sum.ml", "", True, 1],  # OK - 6 failed
 
     # EVEN TREE
-    # ["incomplete/eventree/sum_of_parities.ml", "", True, 1],  # 2 Failures
-    # ["incomplete/eventree/parity_of_max.ml", "", True, 1],  # Error
+    ["incomplete/eventree/sum_of_parities.ml", "", True, 1],  # 2 Failures
+    ["incomplete/eventree/parity_of_max.ml", "", True, 1],  # Error
 
     # EXPRESSIONS
-    # ["incomplete/expressions/norm_arith.ml", "", True, 1],  # OK
-    # ["incomplete/expressions/max_subexpr_sum.ml",
-    #     "", True, 1],  # OK 8 failures
+    ["incomplete/expressions/norm_arith.ml", "", True, 1],  # OK
+    ["incomplete/expressions/max_subexpr_sum.ml",
+     "", True, 1],  # OK 8 failures
 
     # INDEXED LIST
-    # ["incomplete/indexed_list/search.ml", "", True, 1],  # OK
-    # ["incomplete/indexed_list/position_polynomial.ml", "", True, 1],  # OK
-    # ["incomplete/indexed_list/sum_elts_lt_pos.ml", "", True, 1],  # OK
+    ["incomplete/indexed_list/search.ml", "", True, 1],  # OK
+    ["incomplete/indexed_list/position_polynomial.ml", "", True, 1],  # OK
+    ["incomplete/indexed_list/sum_elts_lt_pos.ml", "", True, 1],  # OK
 
     # LISTS
-    # ["incomplete/list/sum.ml", "", True, 1],  # OK
-    # ["incomplete/list/mps.ml", "", True, 1],  # OK
-    # ["incomplete/list/mss.ml", "", True, 1],  # OK
-    # ["incomplete/list/mts.ml", "", True, 1],  # OK
-    # ["incomplete/list/alist_sum.ml", "", True, 1],  # OK
-    # ["incomplete/list/atoi.ml", "", True, 1],  # OK
-    # ["incomplete/list/bal.ml", "", True, 1],  # OK
-    # ["incomplete/list/block_ones_extremas.ml", "-n 20", True, 1],  # OK
-    # ["incomplete/list/hamming.ml", "-NB", True, 1],  # OK
-    # ["incomplete/list/last.ml", "", True, 1],  # OK
-    # ["incomplete/list/len.ml", "", True, 1],  # OK
-    # ["incomplete/list/line_of_sight.ml", "", True, 1],  # OK
-    # ["incomplete/list/maxcount.ml", "", True, 1],  # OK
-    # ["incomplete/list/max_block_ones.ml", "", True, 1],  # OK
-    # ["incomplete/list/max.ml", "", True, 1],  # OK
-    # ["incomplete/list/min.ml", "", True, 1],  # OK
-    # ["incomplete/list/mincount.ml", "", True, 1],  # OK
-    # ["incomplete/list/mts_and_mps_hom.pmrs", "", True, 1],  # OK
-    # ["incomplete/list/poly.ml", "", True, 1],  # OK
-    # ["incomplete/list/prod.ml", "", True, 1],  # OK
-    # ["incomplete/list/search.ml", "", True, 1],  # OK
-    # ["incomplete/list/slsp.ml", "", True, 1],  # OK
-    # ["incomplete/list/sndmax.ml", "", True, 1],  # OK
-    # ["incomplete/list/sndmin.ml", "", True, 1],  # OK
-    # ["incomplete/list/sumgt.ml", "", True, 1],  # OK
-    # ["incomplete/list/sumodds.ml", "", True, 1],  # OK
-    # ["incomplete/list/sumevens.ml", "", True, 1],  # OK
-    # ["incomplete/list/zero_after_one.ml", "", True, 1],  # OK
-    # ["incomplete/list/zeros_ones.ml", "", True, 1],  # OK
+    ["incomplete/list/sum.ml", "", True, 1],  # OK
+    ["incomplete/list/mps.ml", "", True, 1],  # OK
+    ["incomplete/list/mss.ml", "", True, 1],  # OK
+    ["incomplete/list/mts.ml", "", True, 1],  # OK
+    ["incomplete/list/alist_sum.ml", "", True, 1],  # OK
+    ["incomplete/list/atoi.ml", "", True, 1],  # OK
+    ["incomplete/list/bal.ml", "", True, 1],  # OK
+    ["incomplete/list/block_ones_extremas.ml", "-n 20", True, 1],  # OK
+    ["incomplete/list/hamming.ml", "-NB", True, 1],  # OK
+    ["incomplete/list/last.ml", "", True, 1],  # OK
+    ["incomplete/list/len.ml", "", True, 1],  # OK
+    ["incomplete/list/line_of_sight.ml", "", True, 1],  # OK
+    ["incomplete/list/maxcount.ml", "", True, 1],  # OK
+    ["incomplete/list/max_block_ones.ml", "", True, 1],  # OK
+    ["incomplete/list/max.ml", "", True, 1],  # OK
+    ["incomplete/list/min.ml", "", True, 1],  # OK
+    ["incomplete/list/mincount.ml", "", True, 1],  # OK
+    ["incomplete/list/mts_and_mps_hom.pmrs", "", True, 1],  # OK
+    ["incomplete/list/poly.ml", "", True, 1],  # OK
+    ["incomplete/list/prod.ml", "", True, 1],  # OK
+    ["incomplete/list/search.ml", "", True, 1],  # OK
+    ["incomplete/list/slsp.ml", "", True, 1],  # OK
+    ["incomplete/list/sndmax.ml", "", True, 1],  # OK
+    ["incomplete/list/sndmin.ml", "", True, 1],  # OK
+    ["incomplete/list/sumgt.ml", "", True, 1],  # OK
+    ["incomplete/list/sumodds.ml", "", True, 1],  # OK
+    ["incomplete/list/sumevens.ml", "", True, 1],  # OK
+    ["incomplete/list/zero_after_one.ml", "", True, 1],  # OK
+    ["incomplete/list/zeros_ones.ml", "", True, 1],  # OK
 
     # LIST TO TREE
-    # ["incomplete/list_to_tree/search.ml", "", True, 1],  # OK
-    # ["incomplete/list_to_tree/search_v2.ml", "", True, 1],  # OK
-    # ["incomplete/list_to_tree/search_v3.ml", "", True, 1],  # OK
-    # ["incomplete/list_to_tree/mls.ml", "", True, 1],  # OK
+    ["incomplete/list_to_tree/search.ml", "", True, 1],  # OK
+    ["incomplete/list_to_tree/search_v2.ml", "", True, 1],  # OK
+    ["incomplete/list_to_tree/search_v3.ml", "", True, 1],  # OK
+    ["incomplete/list_to_tree/mls.ml", "", True, 1],  # OK
 
     # MEMOIZATION
-    # ["incomplete/memo/constant.ml", "", True, 1],  # OK
-    # ["incomplete/memo/count_lt.ml", "", True, 1],  # Too many open files
-    # ["incomplete/memo/max_contains.ml", "", True, 1],  # Too many open files
-    # ["incomplete/memo/max_sum_gt.ml", "", True, 1],  # Too many open files
-    # ["incomplete/memo/minmax.ml", "", True, 1],  # OK
-    # ["incomplete/memo/mts_memo_sum.ml", "", True, 1],  # OK
-    # ["incomplete/memo/proper_indexation_sum_lt_pos_v2.ml",
-    #     "", True, 1],  # ENd of file error
-    # ["incomplete/memo/proper_indexation_sum_lt_pos.ml",
-    #     "", True, 1],  # end of file error
-    # ["incomplete/memo/tree_size.ml", "", True, 1],  # ??
+    ["incomplete/memo/constant.ml", "", True, 1],  # OK
+    ["incomplete/memo/count_lt.ml", "", True, 1],  # Too many open files
+    ["incomplete/memo/max_contains.ml", "", True, 1],  # Too many open files
+    ["incomplete/memo/max_sum_gt.ml", "", True, 1],  # Too many open files
+    ["incomplete/memo/minmax.ml", "", True, 1],  # OK
+    ["incomplete/memo/mts_memo_sum.ml", "", True, 1],  # OK
+    ["incomplete/memo/proper_indexation_sum_lt_pos_v2.ml",
+     "", True, 1],  # ENd of file error
+    ["incomplete/memo/proper_indexation_sum_lt_pos.ml",
+     "", True, 1],  # end of file error
+    ["incomplete/memo/tree_size.ml", "", True, 1],  # ??
 
     # MISC
-    # ["incomplete/misc/composed_unkwns.ml", "", True, 1],  # OK
-    # ["incomplete/misc/unknowns_are_ids.ml", "", True, 1],  # OK
-    # ["incomplete/misc/simple_nnf.ml", "", True, 1],  # OK
-    # ["incomplete/misc/unknowns_are_ids.ml", "", True, 1],  # OK
-    # ["incomplete/misc/count_between.ml", "", True, 1],  # OK
+    ["incomplete/misc/composed_unkwns.ml", "", True, 1],  # OK
+    ["incomplete/misc/unknowns_are_ids.ml", "", True, 1],  # OK
+    ["incomplete/misc/simple_nnf.ml", "", True, 1],  # OK
+    ["incomplete/misc/unknowns_are_ids.ml", "", True, 1],  # OK
+    ["incomplete/misc/count_between.ml", "", True, 1],  # OK
 
 
     # SORTED LIST
-    # ["incomplete/sortedlist/count_lt_p1.ml", "", True, 1],  # Unknown error
-    # ["incomplete/sortedlist/exists_equal_elems.ml", "", True, 1],  # ??
-    # ["incomplete/list/sum_p1.ml", "", True, 1],  # OK
-    # ["incomplete/sortedlist/failing_slspplus.ml", ""],  # With 4 failures
-    # ["incomplete/list/sorted_min.ml", "", True, 1],  # OK
-    # ["incomplete/list/swap_game.ml", "", False, 1],  # OK
-    # ["incomplete/sortedlist/common_elt.ml", "", False, 1],  # OK
+    ["incomplete/sortedlist/count_lt_p1.ml", "", True, 1],  # Unknown error
+    ["incomplete/sortedlist/exists_equal_elems.ml", "", True, 1],  # ??
+    ["incomplete/list/sum_p1.ml", "", True, 1],  # OK
+    ["incomplete/sortedlist/failing_slspplus.ml", ""],  # With 4 failures
+    ["incomplete/list/sorted_min.ml", "", True, 1],  # OK
+    ["incomplete/list/swap_game.ml", "", False, 1],  # OK
+    ["incomplete/sortedlist/common_elt.ml", "", False, 1],  # OK
     ["incomplete/sortedlist/index_of.ml", "", True, 1],  # OK
     ["incomplete/sortedlist/interval_intersection.ml", ""],  # OK
     ["incomplete/sortedlist/is_intersection_empty.ml", ""],  # ??
